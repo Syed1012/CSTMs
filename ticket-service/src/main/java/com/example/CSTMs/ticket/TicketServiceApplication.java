1fccfc03d5225cdf717de81fe2ffffe33b2d60e6
Syed1012 <syedmusaib66@gmail.com>
2025-02-06 11:37:23 +0530

    secured Mongo URL through .env

--- ticket-service/src/main/java/com/example/CSTMs/ticket/TicketServiceApplication.java
+++ ticket-service/src/main/java/com/example/CSTMs/ticket/TicketServiceApplication.java
@@ -1,12 +1,21 @@
 package com.example.CSTMs.ticket;
 
+import java.util.Map;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+import io.github.cdimascio.dotenv.Dotenv;
+
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
 public class TicketServiceApplication {
     public static void main(String[] args) {
+
+        // Load .env variables
+        Dotenv dotenv = Dotenv.load();
+        System.setProperty("MONGO_URI", dotenv.get("MONGO_URI"));
+
         SpringApplication.run(TicketServiceApplication.class, args);
     }
 }
