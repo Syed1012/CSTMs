1fcaf4e7f4a6f9b01e151619e7ca4f7647494c6c
Syed1012 <syedmusaib66@gmail.com>
2025-02-06 11:54:57 +0530

    Removed Unnecessary lines of code

--- ticket-service/src/main/java/com/example/CSTMs/ticket/TicketServiceApplication.java
+++ ticket-service/src/main/java/com/example/CSTMs/ticket/TicketServiceApplication.java
@@ -1,7 +1,5 @@
 package com.example.CSTMs.ticket;
 
-import java.util.Map;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
